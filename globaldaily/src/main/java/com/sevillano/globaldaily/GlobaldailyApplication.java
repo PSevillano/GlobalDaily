1b340224a6edd6d7526bad1af9f50a1f5532d581
Pol <polsevillano@gmail.com>
2022-01-03 20:09:45 +0100

    Primer commit en develop

---
+++ globaldaily/src/main/java/com/sevillano/globaldaily/GlobaldailyApplication.java
@@ -0,0 +1,13 @@
+package com.sevillano.globaldaily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobaldailyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobaldailyApplication.class, args);
+	}
+
+}
---
+++ globaldaily/src/test/java/com/sevillano/globaldaily/GlobaldailyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sevillano.globaldaily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobaldailyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
